6d966b6592615e69fc1ceff5624c18e9c621f817
Stoffels84 <info@solutionsforyou.be>
2026-05-15 14:57:25 +0200

    feat: Lock match predictions after start time
    Prevent users from changing predictions once a match has begun. Admins retain the ability to modify predictions regardless of the match start time.

--- src/App.tsx
+++ src/App.tsx
@@ -1718,7 +1718,7 @@ const MatchCard: React.FC<{
     }
   }, [prediction, saving, showSuccess]);
 
-  const isLocked = new Date().getTime() > new Date(match.date).getTime() - (60 * 60 * 1000);
+  const isLocked = !isAdmin && new Date().getTime() > new Date(match.date).getTime();
 
   const matchPredictions = allPredictions.filter(p => p.matchId === match.id);
   const totalPredictions = matchPredictions.length;
